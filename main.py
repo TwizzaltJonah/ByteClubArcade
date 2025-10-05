9844f63ffad58b382a207f428236192052f3d72a
TwizzaltJonah <Jonah@Jaylan.net>
2025-10-05 16:32:54 -0500

    simple perfomance test with image scaling and rotation and a simple fps counter

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+import math
 
-print("Hello World!")
+import graphics as g
+import time
+
+last60frametime = time.time()
+frameNum = 0
+
+win = g.Window(600, 400)
+
+img = g.Image(300, 200, win, "test.png", True)
+img.resizeImage(160, 160)
+
+
+while win.running:
+    scale = math.sin(time.time() * 1.5) * .5 + 1.5
+    img.resizeImage(160 * scale, 160 * scale)
+    img.rotateImageTo(math.sin(time.time() * 3) * 20)
+
+    img.draw()
+    win.update()
+
+    frameNum += 1
+    if frameNum % 60 == 0:
+        t = time.time()
+        print(60 / (t - last60frametime))
+        last60frametime = t
